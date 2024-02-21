f425a04516e2392b6dce85f3893ac5e492542b5c
Rahela Mandici <88371164+RahelaM@users.noreply.github.com>
2024-02-21 13:47:56 +0200

    CSV files parser (#13)

---
+++ src/lib/services/papa-parse/index.ts
@@ -0,0 +1,45 @@
+import Papa from 'papaparse';
+
+function getCSVHeader(file: File): Promise<Papa.ParseResult<string>> {
+  return new Promise((resolve, reject) => {
+    Papa.parse(file, {
+      skipEmptyLines: true,
+      header: true,
+      preview: 1, // Only parse the first row
+      complete: (results) => {
+        if (results.meta.fields) {
+          resolve({
+            ...results,
+            data: results.meta.fields,
+          });
+        }
+        reject(new Error('No headers found in CSV file.'));
+      },
+      error: (error) => {
+        reject(error);
+      },
+    });
+  });
+}
+
+function parseCSV(
+  file: File,
+  withHeaders: boolean = true,
+): Promise<Papa.ParseResult<unknown>> {
+  return new Promise((resolve, reject) => {
+    Papa.parse(file, {
+      header: withHeaders,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: (results) => {
+        resolve(results);
+      },
+      error: (error) => {
+        reject(error);
+      },
+    });
+  });
+}
+
+const papaParser = { getCSVHeader, parseCSV };
+export default papaParser;
