2282c005f80ef309a63ff87a5ceea80322effc67
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-02-27 15:45:27 +0200

    enforce content or status

--- src/app/api/dataset/edit/cell/schema.ts
+++ src/app/api/dataset/edit/cell/schema.ts
@@ -1,8 +1,12 @@
 import { ENUM_Ground_truth_status } from '@/lib/types';
 import { z } from 'zod';
 
-export const editDatasetCellSchema = z.object({
-  groundTruthCellId: z.number(),
-  content: z.string(),
-  status: z.optional(z.nativeEnum(ENUM_Ground_truth_status)),
-});
+export const editDatasetCellSchema = z
+  .object({
+    groundTruthCellId: z.number(),
+    content: z.string().optional(),
+    status: z.nativeEnum(ENUM_Ground_truth_status).optional(),
+  })
+  .refine((data) => data.content || data.status, {
+    message: "Either 'content' or 'status' must be provided",
+  });
