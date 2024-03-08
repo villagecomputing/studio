b6fd80e6d24bb469f13932de39e416697b55dc26
Alexandru Parpalea <alexandru.parpalea@envoy.com>
2024-03-08 15:48:37 +0200

    use infered types

--- src/lib/services/ApiUtils/dataset/uploadToDb.ts
+++ src/lib/services/ApiUtils/dataset/uploadToDb.ts
@@ -1,17 +1,9 @@
+import { ApiEndpoints, PayloadSchemaType } from '@/lib/routes/routes';
+
 // TODO: may need to expand to include labelkit specific columns common for all datasets
-type DatasetRow = {
-  columnName: string;
-  columnValue: string;
-  columnType?: string;
-}[];
-
-type Dataset = {
-  organizationId: number;
-  datasetIdentifier: string;
-  datasetRows: DatasetRow[];
-};
-
-export async function saveDatasetDetailsAsTable(_dataset: Dataset) {
-  // TODO: implement
+export async function saveDatasetDetailsAsTable(
+  _dataset: PayloadSchemaType[ApiEndpoints.datasetUploadAsTable],
+) {
+  // TODO: implement + parse
   throw new Error('Not implemented');
 }
