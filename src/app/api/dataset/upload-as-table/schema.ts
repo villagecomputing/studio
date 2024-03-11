ddde0cd00696b71f9e07fc876509f079ea479f9d
Alexandru Parpalea <alexandru.parpalea@envoy.com>
2024-03-11 13:37:41 +0200

    create table if not exists, insert rows

--- src/app/api/dataset/upload-as-table/schema.ts
+++ src/app/api/dataset/upload-as-table/schema.ts
@@ -4,12 +4,10 @@ export const uploadDatasetRowPayloadSchema = z.array(
   z.object({
     columnName: z.string(),
     columnValue: z.string(),
-    columnType: z.optional(z.string()),
   }),
 );
 
 export const uploadDatasetAsTablePayloadSchema = z.object({
-  organizationId: z.number(),
-  datasetIdentifier: z.string(),
+  datasetName: z.string(),
   datasetRows: z.array(uploadDatasetRowPayloadSchema),
 });
--- src/lib/services/ApiUtils/dataset/uploadToDb.ts
+++ src/lib/services/ApiUtils/dataset/uploadToDb.ts
@@ -1,9 +1,24 @@
 import { ApiEndpoints, PayloadSchemaType } from '@/lib/routes/routes';
+import PrismaClient, { Prisma } from '../../prisma';
 
 // TODO: may need to expand to include labelkit specific columns common for all datasets
 export async function saveDatasetDetailsAsTable(
-  _dataset: PayloadSchemaType[ApiEndpoints.datasetUploadAsTable],
+  dataset: PayloadSchemaType[ApiEndpoints.datasetUploadAsTable],
 ) {
-  // TODO: implement + parse
-  throw new Error('Not implemented');
+  const columnDefinitions = dataset.datasetRows[0]
+    .map((column) => `"${column.columnName}" TEXT`)
+    .join(', ');
+  const createTabelQuery = Prisma.sql`CREATE TABLE IF NOT EXISTS ${Prisma.raw(`"${dataset.datasetName}"`)} (${Prisma.raw(columnDefinitions)})`;
+
+  await PrismaClient.$executeRaw(createTabelQuery);
+  const columns = dataset.datasetRows[0]
+    .map((column) => `"${column.columnName}"`)
+    .join(', ');
+
+  const values = dataset.datasetRows.map(
+    (row) => `(${row.map((column) => `"${column.columnValue}"`).join(', ')})`,
+  );
+
+  const insertRowsQuery = Prisma.sql`INSERT INTO ${Prisma.raw(`"${dataset.datasetName}"`)} (${Prisma.raw(columns)}) VALUES ${Prisma.raw(values.join(',\n'))}`;
+  await PrismaClient.$executeRaw(insertRowsQuery);
 }
