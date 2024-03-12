81974d8e8d874e21069c29f75c4d07d0a006db12
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-03-12 17:07:04 +0200

    database utils

---
+++ src/lib/services/DatabaseUtils/update.ts
@@ -0,0 +1,32 @@
+import { Prisma } from '@prisma/client';
+import PrismaClient from '../prisma';
+import { assertTableExists } from './common';
+
+export async function update<T>(
+  tableName: string,
+  setValues: { [key: string]: string },
+  whereConditions?: { [key: string]: string },
+): Promise<number> {
+  await assertTableExists(tableName);
+
+  let sqlQuery = Prisma.sql`UPDATE "${Prisma.raw(tableName)}" SET `;
+  const setClauses = Object.entries(setValues).map(([key, value]) => {
+    return `"${key}" = ${Prisma.raw(value)}`;
+  });
+  sqlQuery = Prisma.sql`${sqlQuery} ${Prisma.join(setClauses)}`;
+
+  if (whereConditions) {
+    const whereClauses = Object.entries(whereConditions).map(([key, value]) => {
+      return `"${key}" = ${Prisma.raw(value)}`;
+    });
+    sqlQuery = Prisma.sql`${sqlQuery} WHERE ${Prisma.join(whereClauses)}`;
+  }
+
+  try {
+    const result = await PrismaClient.$executeRaw<T>(sqlQuery);
+    return result;
+  } catch (error) {
+    console.error('Error executing raw SQL update:', error);
+    throw error;
+  }
+}
