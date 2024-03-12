01fa0042e1e487083bf30dbebc25dfdd1e051e4a
Sebastian Tucaliuc <sebastian.tucaliuc@amplified.software>
2024-03-12 15:58:08 +0200

    Get Dataset details (#70)

--- src/lib/services/ApiUtils/index.ts
+++ src/lib/services/ApiUtils/index.ts
@@ -1,9 +1,6 @@
-import {
-  approveAll,
-  editDatasetCell,
-  editDatasetColumn,
-  getDataset,
-} from './dataset';
+import { approveAll, editDatasetCell, editDatasetColumn } from './dataset';
+
+import { getDataset } from './dataset/getDataset';
 
 import { isFilenameAvailable, saveDatasetDetails } from './dataset/upload';
 import { saveDatasetDetailsAsTable } from './dataset/uploadToDb';
