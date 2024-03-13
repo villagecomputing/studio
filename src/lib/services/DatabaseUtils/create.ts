fa6edb15d7f0f14dbc5f515416bb63581b17ecfc
Alexandru Parpalea <alexandru.parpalea@envoy.com>
2024-03-13 12:46:25 +0200

    add rawsql for table creation

---
+++ src/lib/services/DatabaseUtils/create.ts
@@ -0,0 +1,26 @@
+import { Prisma } from '@prisma/client';
+import PrismaClient from '../prisma';
+import { assertTableExists } from './common';
+
+export async function create<T>(
+  tableName: string,
+  columns: string[],
+): Promise<number> {
+  await assertTableExists(tableName);
+
+  const idColumnDefinition = Prisma.sql`"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT`;
+  const columnDefinitionStringArray = columns.map(
+    (column) => Prisma.sql`"${column}" TEXT`,
+  );
+
+  const sqlQuery = Prisma.sql`CREATE TABLE "${Prisma.raw(tableName)}"
+  (${Prisma.raw([idColumnDefinition, ...columnDefinitionStringArray].join(', '))})`;
+
+  try {
+    const result = await PrismaClient.$executeRaw<T>(sqlQuery);
+    return result;
+  } catch (error) {
+    console.error('Error executing raw SQL create:', error);
+    throw error;
+  }
+}
