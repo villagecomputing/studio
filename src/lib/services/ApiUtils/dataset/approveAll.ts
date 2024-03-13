c33470ec65e4a8d62fdb767a1b04013f8dd2129e
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-03-13 14:27:41 +0200

    fix approve all

--- src/lib/services/ApiUtils/dataset/approveAll.ts
+++ src/lib/services/ApiUtils/dataset/approveAll.ts
@@ -1,16 +1,23 @@
 import { ApiEndpoints, PayloadSchemaType } from '@/lib/routes/routes';
-import { getDatasetNameAndGTColumnField } from './utils';
+import { ENUM_Ground_truth_status } from '@/lib/types';
+import DatabaseUtils from '../../DatabaseUtils';
+import {
+  getDatasetNameAndGTColumnField,
+  getGroundTruthStatusColumnName,
+} from './utils';
 
 export async function approveAll(
   payload: PayloadSchemaType[ApiEndpoints.datasetApproveAll],
 ) {
   const { datasetId } = payload;
   try {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { datasetName, groundTruthColumnField } =
       await getDatasetNameAndGTColumnField(datasetId);
 
-    // TODO: update ${groundTruthColumnField} in ${datasetName} with {status: ENUM_Ground_truth_status.APPROVED}
+    await DatabaseUtils.update(datasetName, {
+      [getGroundTruthStatusColumnName(groundTruthColumnField)]:
+        ENUM_Ground_truth_status.APPROVED,
+    });
   } catch (error) {
     console.error(error);
     throw new Error('Error updating all ground truth cells status');
