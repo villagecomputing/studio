989f5340b823816dfcffdd16997a18c4b05eb282
Alexandru Parpalea <alexandru.parpalea@envoy.com>
2024-03-13 16:39:52 +0200

    PR suggestions

--- src/lib/services/ApiUtils/dataset/newDataset.ts
+++ src/lib/services/ApiUtils/dataset/newDataset.ts
@@ -1,96 +1,9 @@
-import { getColumnFieldFromNameAndIndex } from '@/app/(authenticated)/data/[datasetId]/utils/commonUtils';
 import { newDatasetPayloadSchema } from '@/app/api/dataset/new/schema';
 import { ApiEndpoints, PayloadSchemaType } from '@/lib/routes/routes';
-import { ENUM_Column_type } from '@/lib/types';
-import { PrismaClient } from '@prisma/client';
+
 import DatabaseUtils from '../../DatabaseUtils';
-import { ColumnDefinition, ColumnType } from '../../DatabaseUtils/types';
-import { getGroundTruthStatusColumnName } from './utils';
-
-type DatasetField = {
-  displayName: string;
-  sanitizedName: string;
-  index: number;
-  type: ENUM_Column_type;
-};
-
-function buildDatasetFields(
-  columns: string[],
-  groundTruths: string[],
-): DatasetField[] {
-  const datasetFields: DatasetField[] = [
-    {
-      displayName: 'id',
-      sanitizedName: 'id',
-      index: -1,
-      type: ENUM_Column_type.IDENTIFIER,
-    },
-  ];
-
-  for (let i = 0; i < columns.length; i++) {
-    datasetFields.push({
-      displayName: columns[i],
-      sanitizedName: getColumnFieldFromNameAndIndex(columns[i], i),
-      index: i,
-      type: ENUM_Column_type.INPUT,
-    });
-  }
-
-  const groundTruthSanitizedsNames: string[] = [];
-  for (let i = 0; i < groundTruths.length; i++) {
-    const index = i + columns.length;
-    const sanitizedName = getColumnFieldFromNameAndIndex(
-      groundTruths[index],
-      index,
-    );
-    groundTruthSanitizedsNames.push(sanitizedName);
-    datasetFields.push({
-      displayName: groundTruths[i],
-      sanitizedName: sanitizedName,
-      index: index + columns.length,
-      type: ENUM_Column_type.GROUND_TRUTH,
-    });
-  }
-
-  for (let i = 0; i < groundTruths.length; i++) {
-    datasetFields.push({
-      displayName: `${groundTruths[i]} status`,
-      sanitizedName: getGroundTruthStatusColumnName(
-        groundTruthSanitizedsNames[i],
-      ),
-      index: -1,
-      type: ENUM_Column_type.GROUND_TRUTH_STATUS,
-    });
-  }
-
-  return datasetFields;
-}
-
-function buildDatasetColumnDefinition(
-  datasetFields: DatasetField[],
-): ColumnDefinition[] {
-  return datasetFields.map((field) => {
-    switch (field.type) {
-      case ENUM_Column_type.IDENTIFIER:
-        return {
-          isAutoincrement: true,
-          type: ColumnType.INTEGER,
-          isNullable: false,
-          name: field.sanitizedName,
-          isPrimaryKey: true,
-        };
-      case ENUM_Column_type.GROUND_TRUTH:
-      case ENUM_Column_type.GROUND_TRUTH_STATUS:
-      case ENUM_Column_type.INPUT:
-        return {
-          type: ColumnType.TEXT,
-          name: field.sanitizedName,
-        };
-      default:
-        throw new Error(`Unupported field type: ${field.type}`);
-    }
-  });
-}
+import { PrismaClient } from '../../prisma';
+import { buildDatasetColumnDefinition, buildDatasetFields } from './utils';
 
 export async function newDataset(
   payload: PayloadSchemaType[ApiEndpoints.datasetNew],
@@ -122,17 +35,19 @@ export async function newDataset(
 
   // TODO: update logic to use future executeRaw for bulk insert
   // Update columns table
-  for (const field of datasetFields) {
-    await PrismaClient.column.create({
-      data: {
-        dataset_id: dataset.id,
-        name: field.displayName,
-        field: field.sanitizedName,
-        index: field.index,
-        type: field.type,
-      },
-    });
-  }
-
+  await Promise.all(
+    datasetFields.map(
+      async (field) =>
+        await PrismaClient.column.create({
+          data: {
+            dataset_id: dataset.id,
+            name: field.name,
+            field: field.field,
+            index: field.index,
+            type: field.type,
+          },
+        }),
+    ),
+  );
   return dataset.id;
 }
--- src/lib/services/ApiUtils/dataset/utils.ts
+++ src/lib/services/ApiUtils/dataset/utils.ts
@@ -1,5 +1,12 @@
-import { ENUM_Column_type } from '@/lib/types';
+import { getColumnFieldFromNameAndIndex } from '@/app/(authenticated)/data/[datasetId]/utils/commonUtils';
+import { exhaustiveCheck, guardStringEnum } from '@/lib/typeUtils';
+import { ENUM_Column_type, ENUM_Ground_truth_status } from '@/lib/types';
+import { Column } from '@prisma/client';
+import _ from 'lodash';
+import { ColumnDefinition, ColumnType } from '../../DatabaseUtils/types';
 import PrismaClient from '../../prisma';
+
+type DatasetField = Pick<Column, 'name' | 'field' | 'index' | 'type'>;
 
 export const getDatasetNameAndGTColumnField = async (
   datasetId: number,
@@ -24,3 +31,91 @@ export const getGroundTruthStatusColumnName = (
 ) => {
   return `${groundTruthColumnName}_STATUS`;
 };
+
+export function buildDatasetFields(
+  columns: string[],
+  groundTruths: string[],
+): DatasetField[] {
+  const datasetFields: DatasetField[] = [
+    {
+      name: 'id',
+      field: 'id',
+      index: -1,
+      type: ENUM_Column_type.IDENTIFIER,
+    },
+  ];
+  columns.map((value, index) =>
+    datasetFields.push({
+      name: value,
+      field: getColumnFieldFromNameAndIndex(value, index),
+      index: index,
+      type: ENUM_Column_type.INPUT,
+    }),
+  );
+
+  const groundTruthSanitizedsNames: string[] = [];
+  groundTruths.map((value, index) => {
+    const newIndex = index + columns.length;
+    const sanitizedName = getColumnFieldFromNameAndIndex(
+      groundTruths[newIndex],
+      index,
+    );
+    groundTruthSanitizedsNames.push(sanitizedName);
+    datasetFields.push({
+      name: value,
+      field: sanitizedName,
+      index: newIndex,
+      type: ENUM_Column_type.GROUND_TRUTH,
+    });
+  });
+
+  groundTruths.map((value, index) =>
+    datasetFields.push({
+      name: `${value} status`,
+      field: getGroundTruthStatusColumnName(groundTruthSanitizedsNames[index]),
+      index: -1,
+      type: ENUM_Column_type.GROUND_TRUTH_STATUS,
+    }),
+  );
+
+  return datasetFields;
+}
+
+export function buildDatasetColumnDefinition(
+  datasetFields: DatasetField[],
+): ColumnDefinition[] {
+  return _(
+    datasetFields.map((field) => {
+      const type = guardStringEnum(ENUM_Column_type, field.type);
+      switch (type) {
+        case ENUM_Column_type.IDENTIFIER:
+          return {
+            isAutoincrement: true,
+            type: ColumnType.INTEGER,
+            isNullable: false,
+            name: field.field,
+            isPrimaryKey: true,
+          };
+        case ENUM_Column_type.GROUND_TRUTH:
+        case ENUM_Column_type.INPUT:
+          return {
+            type: ColumnType.TEXT,
+            name: field.field,
+          };
+        case ENUM_Column_type.GROUND_TRUTH_STATUS:
+          return {
+            type: ColumnType.TEXT,
+            name: field.field,
+            typeCheckValues: Object.values(ENUM_Ground_truth_status),
+            defaultValue: ENUM_Ground_truth_status.PENDING,
+          };
+        case ENUM_Column_type.PREDICTIVE_LABEL:
+          throw new Error(`Unupported field type: ${field.type}`);
+        default:
+          exhaustiveCheck(type);
+      }
+    }),
+  )
+    .compact()
+    .value();
+}
