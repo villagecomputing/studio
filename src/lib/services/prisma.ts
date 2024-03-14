2b9f38540e8d50c69412a6c343ebb39dbb6107f9
Rahela Mandici <88371164+RahelaM@users.noreply.github.com>
2024-03-14 10:56:22 +0200

    Prisma Client - single instance (#82)

--- src/lib/services/prisma.ts
+++ src/lib/services/prisma.ts
@@ -1,7 +1,18 @@
-// prisma.ts
-
 import { PrismaClient as PrismaClientInit } from '@prisma/client';
 
-export const PrismaClient = new PrismaClientInit();
+const prismaClientSingleton = () => {
+  return new PrismaClientInit();
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const PrismaClient = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = PrismaClient;
+}
 
 export default PrismaClient;
