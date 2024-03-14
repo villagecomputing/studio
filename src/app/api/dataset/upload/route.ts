ea3ed71995728fb04788a5b055fb24bd82125b53
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-03-14 10:14:46 +0200

    remove some comments

--- src/app/api/dataset/upload/route.ts
+++ src/app/api/dataset/upload/route.ts
@@ -1,4 +1,3 @@
-// Imports necessary modules and schemas
 import { ApiEndpoints, PayloadSchemaType } from '@/lib/routes/routes';
 import ApiUtils from '@/lib/services/ApiUtils';
 import DatasetParser from '@/lib/services/DatasetParser';
@@ -9,22 +8,17 @@ import { uploadDatasetPayloadSchema } from './schema';
 
 export async function POST(request: Request) {
   try {
-    // Checks for the correct 'Content-Type' in request headers
     if (!request.headers.get('Content-Type')?.includes('multipart/form-data')) {
       return response('Invalid request headers type', 400);
     }
 
-    // Retrieves FormData from the request
     const formData = await request.formData();
     const file = formData.get('file');
     const requestDatasetData = formData.get('datasetData');
 
-    // Validates the presence of 'file' and 'datasetData' in the FormData
     if (!file || !requestDatasetData) {
       return response('Missing required data', 400);
     }
-
-    // Ensures 'datasetData' is a string before parsing
     if (typeof requestDatasetData !== 'string') {
       return response('Invalid request dataset type', 400);
     }
