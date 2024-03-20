f90e57786db7b651f31e99afd32d1eac082b3de9
Rahela Mandici <rahela.mandici@amplified.software>
2024-03-20 14:26:26 +0200

    replace dataset id with uuid

--- prisma/migrations/20240311111528_init/migration.sql
+++ prisma/migrations/20240311111528_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Dataset_column" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "dataset_id" INTEGER NOT NULL,
+    "dataset_uuid" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "field" TEXT NOT NULL,
     "index" INTEGER NOT NULL,
@@ -15,12 +15,12 @@ CREATE TABLE "Dataset_column" (
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" DATETIME NOT NULL,
     "deleted_at" DATETIME,
-    CONSTRAINT "Column_dataset_id_fkey" FOREIGN KEY ("dataset_id") REFERENCES "Dataset" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "Column_dataset_id_fkey" FOREIGN KEY ("dataset_uuid") REFERENCES "Dataset" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Dataset" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uuid" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL UNIQUE,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deleted_at" DATETIME,
