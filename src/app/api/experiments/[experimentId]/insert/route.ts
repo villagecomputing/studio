d34eda0282dc3fb6b083dc771572bb67aa9753b9
Alexandru Parpalea <alexandru.parpalea@envoy.com>
2024-03-21 11:07:36 +0200

    fix endpoint return

--- src/app/api/experiments/[experimentId]/insert/route.ts
+++ src/app/api/experiments/[experimentId]/insert/route.ts
@@ -1,3 +1,4 @@
+import { response } from '@/app/api/utils';
 import ApiUtils from '@/lib/services/ApiUtils';
 import { insertExperimentPayloadSchema } from './schema';
 
@@ -8,15 +9,19 @@ export async function POST(
   const experimentId = params.experimentId;
   const requestBody = await request.json();
   const payload = insertExperimentPayloadSchema.parse(requestBody);
+  try {
+    ApiUtils.createExperimentTable(experimentId, payload);
+    // TODO:
+    // 2) Extract fields from payload:
+    //   a) step[..].name = 'METADATA' type columns
+    //       - 1 row with value = step[..].metadata
+    //   b) step[..].outputs[..].name = 'OUTPUT' type columns
+    //       - 1 row with value = step[..].outputs[..].value
+    // 4) Insert row in experimentId table with values from 2)
 
-  ApiUtils.createExperimentTable(experimentId, payload);
-  // TODO:
-  // 2) Extract fields from payload:
-  //   a) step[..].name = 'METADATA' type columns
-  //       - 1 row with value = step[..].metadata
-  //   b) step[..].outputs[..].name = 'OUTPUT' type columns
-  //       - 1 row with value = step[..].outputs[..].value
-  // 4) Insert row in experimentId table with values from 2)
-
-  throw new Error('Not implemented');
+    return response('Ok');
+  } catch (error) {
+    console.error('Error in newExperiment:', error);
+    return response('Error processing request', 500);
+  }
 }
