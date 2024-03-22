988de80dfe41b970a1df8eb29778c954616fe194
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-03-22 11:22:40 +0200

    move the context

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,6 +1,5 @@
 import { cn } from '@/lib/utils';
 import Navbar from './components/navbar/Navbar';
-import { ExperimentListProvider } from './experiment/components/ExperimentListProvider';
 
 export default function AuthenticatedLayout({
   children,
@@ -8,11 +7,9 @@ export default function AuthenticatedLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ExperimentListProvider>
-      <div className={cn(['flex h-full w-full'])}>
-        <Navbar />
-        <div className={cn(['flex-1 pt-4'])}>{children}</div>
-      </div>
-    </ExperimentListProvider>
+    <div className={cn(['flex h-full w-full'])}>
+      <Navbar />
+      <div className={cn(['flex-1 pt-4'])}>{children}</div>
+    </div>
   );
 }
