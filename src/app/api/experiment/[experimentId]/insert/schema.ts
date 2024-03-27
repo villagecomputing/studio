feeefce62f3fff2f69695e809ce5fe9f7630f0e9
Alexandru Parpalea <alexandru.parpalea@envoy.com>
2024-03-27 10:49:19 +0200

    ensure that step metadata schema always has latency and tokens

--- src/app/api/experiment/[experimentId]/insert/schema.ts
+++ src/app/api/experiment/[experimentId]/insert/schema.ts
@@ -2,7 +2,13 @@ import { z } from 'zod';
 
 export const experimentStepPayloadSchema = z.object({
   name: z.string(),
-  metadata: z.record(z.string(), z.union([z.number(), z.string()])),
+  metadata: z
+    .object({
+      latency: z.number(),
+      input_tokens: z.number(),
+      output_tokens: z.number(),
+    })
+    .and(z.record(z.string(), z.union([z.number(), z.string()]))),
   outputs: z.array(
     z.object({
       name: z.string(),
