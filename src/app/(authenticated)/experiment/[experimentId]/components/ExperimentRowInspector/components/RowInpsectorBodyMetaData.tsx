c4c3cb679e066f188fe60349ee6c579d3e6ee8a9
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-04-03 13:41:39 +0300

    step metadata

---
+++ src/app/(authenticated)/experiment/[experimentId]/components/ExperimentRowInspector/components/RowInpsectorBodyMetaData.tsx
@@ -0,0 +1,59 @@
+import { useMemo } from 'react';
+import { getExperimentRowMetadata } from '../../../utils';
+import MetadataElement, { Enum_Metadata_Type } from '../../MetadataElement';
+import { useExperimentRowInspectorContext } from '../ExperimentRowInspector';
+import { RAW_DATA_SECTION } from '../ExperimentRowInspectorView';
+
+const RowInspectorBodyMetaData = () => {
+  const {
+    rows,
+    inspectorRowIndex,
+    stepMetadataColumns,
+    costP25,
+    costP75,
+    latencyP25,
+    latencyP75,
+  } = useExperimentRowInspectorContext();
+
+  const metadata = useMemo(() => {
+    if (inspectorRowIndex === null) {
+      return null;
+    }
+    const currentRow = rows[inspectorRowIndex];
+    return getExperimentRowMetadata(currentRow, stepMetadataColumns);
+  }, [inspectorRowIndex, rows, stepMetadataColumns]);
+
+  if (!metadata) {
+    return null;
+  }
+
+  return (
+    <div
+      id={RAW_DATA_SECTION}
+      className="flex gap-6 border-y border-border bg-white px-4 py-3 text-agDataColor"
+    >
+      <MetadataElement
+        type={Enum_Metadata_Type.LATENCY90}
+        icon
+        value={metadata.latencySum}
+        p25={latencyP25}
+        p75={latencyP75}
+      />
+      <MetadataElement
+        type={Enum_Metadata_Type.COST}
+        icon
+        value={metadata.costSum}
+        p25={costP25}
+        p75={costP75}
+      />
+      <span>
+        <b>{metadata.inputTokensSum}</b> Input tokens
+      </span>
+      <span>
+        <b>{metadata.outputTokensSum}</b> Output tokens
+      </span>
+    </div>
+  );
+};
+
+export default RowInspectorBodyMetaData;
