7e4217c4abecc6c4c990a39914adf6b2418a9472
Alexandru Parpalea <alexndru.parpalea@amplified.software>
2024-04-08 15:12:11 +0300

    set default updated at value

--- prisma/migrations/20240408094846_add_users_and_permissions_table/migration.sql
+++ prisma/migrations/20240408094846_add_users_and_permissions_table/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "uuid" TEXT NOT NULL PRIMARY KEY,
     "external_id" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME,
+    "updated_at" DATETIME NOT NULL,
     "deleted_at" TEXT
 );
 
@@ -12,7 +12,7 @@ CREATE TABLE "API_key" (
     "user_id" TEXT NOT NULL,
     "key" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME,
+    "updated_at" DATETIME NOT NULL,
     "deleted_at" DATETIME,
     CONSTRAINT "API_key_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("uuid") ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -23,7 +23,7 @@ CREATE TABLE "Experiment_User_Permissions" (
     "experiment_id" TEXT NOT NULL, 
     "permission" TEXT NOT NULL CHECK ("permission" IN ('CREATE', 'EDIT', 'VIEW')),
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME,
+    "updated_at" DATETIME NOT NULL,
     "deleted_at" DATETIME,
     CONSTRAINT "Experiment_User_Permissions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("uuid") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Experiment_User_Permissions_experiment_id_fkey" FOREIGN KEY ("experiment_id") REFERENCES "Experiment"("uuid") ON DELETE CASCADE ON UPDATE CASCADE
@@ -35,7 +35,7 @@ CREATE TABLE "Dataset_User_Permissions" (
     "dataset_id" TEXT NOT NULL, 
     "permission" TEXT NOT NULL CHECK ("permission" IN ('CREATE', 'EDIT', 'VIEW')),
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME,
+    "updated_at" DATETIME NOT NULL,
     "deleted_at" DATETIME,
     CONSTRAINT "Dataset_User_Permissions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("uuid") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Dataset_User_Permissions_dataset_id_fkey" FOREIGN KEY ("dataset_id") REFERENCES "Dataset"("uuid") ON DELETE CASCADE ON UPDATE CASCADE
@@ -47,7 +47,7 @@ CREATE TABLE "Log_User_Permissions" (
     "log_id" TEXT NOT NULL, 
     "permission" TEXT NOT NULL CHECK ("permission" IN ('CREATE', 'EDIT', 'VIEW')),
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME,
+    "updated_at" DATETIME NOT NULL,
     "deleted_at" DATETIME,
     CONSTRAINT "Log_User_Permissions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("uuid") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Log_User_Permissions_log_id_fkey" FOREIGN KEY ("log_id") REFERENCES "Logs"("uuid") ON DELETE CASCADE ON UPDATE CASCADE
