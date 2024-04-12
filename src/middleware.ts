72b01d676a6d26104890668649fe6ea691058a45
Alexandru Parpalea <alexandru.parpalea@amplified.software>
2024-04-12 16:50:53 +0300

    update public routes according to env type

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,8 @@ import { assertApiKeyFormat } from './lib/services/ApiUtils/user/utils';
 
 export default authMiddleware({
   publicRoutes: [
+    process.env.ENV_TYPE === 'local' ? '/(.*)' : '/api-doc',
     '/api/webhook(.*)',
-    '/api-doc',
     // TODO: this endpoint should be removed from public as soon as the UI for getting the API Key is done
     '/api/user/(.*)/getApiKey',
   ],
