bce44366bece64273c596b92ceda7b4926fbaf91
Alexandru Parpalea <alexandru.parpalea@amplified.software>
2024-04-19 11:01:01 +0300

    make prompt and error optional and nullable

--- src/app/api/experiment/[experimentId]/insert/schema.ts
+++ src/app/api/experiment/[experimentId]/insert/schema.ts
@@ -4,12 +4,12 @@ export const experimentStepMetadata = z
   .object({
     latency: z.number(),
     success: z.boolean(),
-    error: z.union([z.string(), z.null()]),
+    error: z.union([z.string(), z.null()]).optional(),
     input_tokens: z.number().optional(),
     output_tokens: z.number().optional(),
     input_cost: z.number().optional(),
     output_cost: z.number().optional(),
-    prompt: z.string().optional(),
+    prompt: z.union([z.string(), z.null()]).optional(),
   })
   .passthrough();
 
