61feade6b9ec0bbbb5f9cffdb77d37b19a98fe11
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-04-23 15:23:56 +0300

    add content type

--- src/lib/services/RichDataParser/types.ts
+++ src/lib/services/RichDataParser/types.ts
@@ -1,3 +1,5 @@
+import { SupportedFormat } from './constants';
+
 export type ComponentProps<T extends object> = T & {
   src: string;
 };
@@ -7,3 +9,13 @@ export type MimeTypeToComponent = {
     props: ComponentProps<T>,
   ) => JSX.Element;
 };
+
+export type CurrentView =
+  | {
+      content: string;
+      type: SupportedFormat.PDF | SupportedFormat.MARKDOWN;
+    }
+  | {
+      content: string[];
+      type: SupportedFormat.IMAGE;
+    };
