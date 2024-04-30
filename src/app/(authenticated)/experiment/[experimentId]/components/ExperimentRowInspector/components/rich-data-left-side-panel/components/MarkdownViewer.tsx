077f0a14e5bb4ca6ab0bd2fc2ba9f179c9546cfd
Sebastian Tucaliuc <sebastian.tucaliuc@gmail.com>
2024-04-30 08:40:10 +0300

    parser

--- src/app/(authenticated)/experiment/[experimentId]/components/ExperimentRowInspector/components/rich-data-left-side-panel/components/MarkdownViewer.tsx
+++ src/app/(authenticated)/experiment/[experimentId]/components/ExperimentRowInspector/components/rich-data-left-side-panel/components/MarkdownViewer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
@@ -5,22 +6,35 @@ type MarkdownViewerProps = {
   content: string;
 };
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ content }) => {
+  const [JSONText, setJSONText] = useState<string | undefined>();
+
+  useEffect(() => {
+    try {
+      setJSONText(JSON.stringify(JSON.parse(content), null, 2));
+    } catch (error) {
+      setJSONText(undefined);
+    }
+  }, [content]);
+
   return (
     <div className="flex-grow overflow-y-scroll px-6 py-4">
       <span className="text-base font-normal text-secondary-foreground">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          components={{
-            a: ({ ...props }) => (
-              <a
-                {...props}
-                className="mb-2 break-all text-primary hover:underline"
-              />
-            ),
-          }}
-        >
-          {content}
-        </ReactMarkdown>
+        {JSONText && <pre>{JSONText}</pre>}
+        {!JSONText && (
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            components={{
+              a: ({ ...props }) => (
+                <a
+                  {...props}
+                  className="mb-2 break-all text-primary hover:underline"
+                />
+              ),
+            }}
+          >
+            {content}
+          </ReactMarkdown>
+        )}
       </span>
     </div>
   );
