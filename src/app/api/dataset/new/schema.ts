6f547d4e2d2fcbdd0801a30b4be15c561140d8e7
AlexParpa <parpalea.alexandru@gmail.com>
2024-04-30 12:55:18 +0300

    2 - Update dynamic dataset coulmn  definition (#217)
    * add dataset and logs uuid as foreign keys to the Dataset and Logs table
    
    * update postgres schema
    
    * regenerate schema
    
    * LF
    
    * CRLF
    
    * update dynamic dataset coulmn  definition
    
    * fix build
    
    * add many to many relationshipt between dataset and logs
    
    * add dataset and logs uuid as foreign keys to the Dataset and Logs table
    
    * update postgres schema
    
    * regenerate schema
    
    * LF
    
    * CRLF
    
    * update dynamic dataset coulmn  definition
    
    * fix build
    
    * remove old migration
    
    * cleanup prisma schema
    
    * cleanup
    
    * cleanup and fixes
    
    * update dataset column type check
    
    * fix deploy
    
    * fix issue with assigning logs rel to dataset
    
    * update swagger
    
    * change index to id

--- src/app/api/dataset/new/schema.ts
+++ src/app/api/dataset/new/schema.ts
@@ -4,6 +4,7 @@ export const newDatasetPayloadSchema = z.object({
   datasetName: z.string(),
   columns: z.array(z.string()),
   groundTruths: z.array(z.string()),
+  logsUuid: z.union([z.string(), z.null()]).optional(),
 });
 
 export const newDatasetResponseSchema = z.object({
