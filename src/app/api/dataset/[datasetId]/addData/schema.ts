c070294f7a193c1b1d1b2950a0e3d4a63888459f
Alexandru Parpalea <alexandru.parpalea@amplified.software>
2024-05-14 12:15:26 +0300

    schema should allow null and number properties

--- src/app/api/dataset/[datasetId]/addData/schema.ts
+++ src/app/api/dataset/[datasetId]/addData/schema.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
 export const addDataPayloadSchema = z.object({
-  datasetRows: z.array(z.record(z.union([z.string(), z.date()]))),
+  datasetRows: z.array(
+    z.record(z.union([z.string(), z.date(), z.null(), z.number()])),
+  ),
 });
